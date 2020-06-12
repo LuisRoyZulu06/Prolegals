57c91a5746bab1b520261022fef54175406e67a9
unknown <johnmfula360@gmail.com>
2020-06-12 09:58:20 +0200

    initial pull

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :prolegals, Prolegals.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "cool",
   database: "prolegals_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
