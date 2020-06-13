e15887244cfadbecde206f57bbdab5bf34a66688
Dolben <dolben800@gmail.com>
2020-06-13 17:39:35 +0200

    davies acount create

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :prolegals, Prolegals.Repo,
   username: "postgres",
-  password: "password06",
+  password: "postgres",
   database: "prolegals",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
@@ -16,7 +16,7 @@ config :prolegals, Prolegals.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :prolegals, ProlegalsWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 5000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
